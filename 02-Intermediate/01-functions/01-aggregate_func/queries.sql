c382a4845d383e8c794e96178769ab5409d8c9d0
Arijit Das <arijitdas.das99@gmail.com>
2026-08-20 20:20:50 +0530

    updated aggregate function

---
+++ 02-Intermediate/01-functions/01-aggregate_func/queries.sql
@@ -0,0 +1,33 @@
+-- aggregate functions
+
+-- sum()
+SELECT sum(salary) AS total_salary FROM users;
+
+-- count()
+SELECT count(*) AS total_records FROM users;
+
+-- avg()
+SELECT avg(salary) AS average_salary FROM users;
+
+-- min()
+SELECT min(age) AS minimum_age FROM users;
+
+-- max()
+SELECT max(age) AS maximum_age FROM users;
+
+
+
+
+-- Usinng GROUP BY clause
+
+-- get the total salary by dept
+SELECT dept, sum(salary) AS total_salary FROM users GROUP BY dept;
+
+-- get how many employee are working in each dept.
+SELECT dept, count(employee) AS total_employee FROM users GROUP BY dept;
+
+-- get the avg salary by dept
+SELECT dept, avg(salary) AS average_salary FROM users GROUP BY dept;
+
+-- get which dept pays the most highest salary.
+SELECT dept FROM users WHERE salary = (SELECT max(salary) AS maximum_salary FROM users);
